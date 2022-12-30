8b28b4591f2df2e69b1e152036b1bb6578160289
FlameFrog <revec1251@gmail.com>
2022-12-30 14:02:17 +0300

    HW4

---
+++ HW_04/Task_02.py
@@ -0,0 +1,18 @@
+# Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+
+N = int(input("Введите число: "))
+i = 2
+mn_list = []
+flag = True
+while flag:
+    if N % i == 0:
+        mn_list.append(i)
+        N = N / i
+        if N == 1: 
+            if len(mn_list) == 1: 
+                print("Число простое и множителей кроме себя самого и 1 у него нет.")
+            else: 
+                print(mn_list)
+            flag = False
+    else: 
+        i += 1
