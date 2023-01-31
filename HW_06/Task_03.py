ef3575111a513727361c47ffb87a6316a4687eba
FlameFrog <revec1251@gmail.com>
2023-01-31 17:58:39 +0300

    chng 2 3rd task

--- HW_06/Task_03.py
+++ HW_06/Task_03.py
@@ -1,8 +1,7 @@
-number = input('Введите любое вещественное число: ')
+number = input("Введите любое вещественное число: ")
 
 list_num = list(map(int, (i for i in number if i.isdigit())))
 
 sum_digit = sum(list_num)
 
-print(f'Исходное число: {number}')
-print(f'Сумма его чисел = {sum_digit}')
+print(f"Сумма его цифр = {sum_digit}")
