bc041d05f2008eea93d2d1f18daad61e8098a9cf
FlameFrog <revec1251@gmail.com>
2023-02-09 18:12:38 +0300

    phonebook v2

--- HW_07/controller.py
+++ HW_07/controller.py
@@ -29,8 +29,7 @@ def made_choice():
     print("Доступные операции с телефонной книгой:\n\
     1 - импорт;\n\
     2 - экспорт;\n\
-    3 - поиск контакта;\n\
-    4 - сортировка.")
+    3 - поиск контакта.")
     while True:
         choice = input("Введите цифру: ")
         if choice == '1':
@@ -49,10 +48,6 @@ def made_choice():
                 print(item[0].center(20), item[1].center(20), item[2].center(20), item[3].center(20), item[4].center(15), item[5].center(30))
             else:
                 print("Данные не обнаружены")
-        elif choice=='4':
-            data = export_data()
-            sorted_by_id = sorted(data, key=lambda tup: tup[1])
-            print_data(sorted_by_id)
         else:
             choice==''
             break    
